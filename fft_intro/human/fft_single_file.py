07b28d6213bc48064f4ac38ee130d5e2174f5670
drameyjoshi <amey.joshi@outlook.com>
2024-02-05 19:55:26 +0000

    Printing summary of unique frequencies.

--- fft_intro/human/fft_single_file.py
+++ fft_intro/human/fft_single_file.py
@@ -3,15 +3,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-fname = "sam/csv/sam_1.csv"
+fname = "aum/csv/aum_1.csv"
 sname = os.path.splitext(os.path.basename(fname))[0]
-sampling_rate = 48000
+aumpling_rate = 48000
+pd.options.mode.copy_on_write = True
 
 dfc = pd.read_csv(fname, header=None)
 X = dfc[0].to_numpy()
 Xhat = np.fft.fft(X)
 N = Xhat.shape[0]
-freq = [f*sampling_rate/N for f in range(0, N)]
+freq = [f*aumpling_rate/N for f in range(0, N)]
 amplitude = np.absolute(Xhat)
 phase = np.angle(Xhat)
 
@@ -22,5 +23,7 @@ significant = df.loc[((df['freq'] <= np.mean(freq)) &
                       (df['amplitude'] >= np.mean(amplitude)))]
 significant.sort_values(by='amplitude', inplace=True)
 uniq_freq = significant.int_freq.unique().astype(np.int64)
-significant.to_csv('sam/csv/sam_df_1.csv', index=False)
-np.savetxt('sam/csv/sam_uf_1.csv', uniq_freq, delimiter=',', fmt='%d')
+significant.to_csv('aum/csv/aum_df_1.csv', index=False)
+np.savetxt('aum/csv/aum_uf_1.csv', uniq_freq, delimiter=',', fmt='%d')
+print(pd.DataFrame(uniq_freq).describe().loc[['mean', 'std', 'min',
+                                              '25%', '50%', '75%', 'max']].T)
