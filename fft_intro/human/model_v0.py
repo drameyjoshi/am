01b47fd34893428ee39a34127249acf5993ea9fb
drameyjoshi <amey.joshi@outlook.com>
2024-02-07 20:38:34 +0000

    Added testing code to the model.

--- fft_intro/human/model_v0.py
+++ fft_intro/human/model_v0.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.utils.validation import column_or_1d
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score
 
 orig_data = pd.read_csv('aum_sam_summary.csv')
 orig_data.columns = ['mean', 'std', 'min', 'q1', 'q2', 'q3', 'max', 'who']
@@ -8,3 +11,16 @@ X = orig_data[['min', 'q1', 'mean', 'q3', 'max']]
 y = column_or_1d(orig_data[['who']])
 clf = LogisticRegression(random_state=0).fit(X, y)
 print(f"Model score = {clf.score(X, y)}.")
+
+test_data = pd.read_csv('test_1_summary.csv')
+test_data.columns = ['mean', 'std', 'min', 'q1', 'q2', 'q3', 'max', 'who']
+X1 = test_data[['min', 'q1', 'mean', 'q3', 'max']]
+y1 = column_or_1d(test_data[['who']])
+yp = clf.predict(X1)
+
+accuracy = accuracy_score(y_true=y1, y_pred=yp)
+print(f"Accuracy score = {accuracy}.")
+cm = confusion_matrix(y_true=y1, y_pred=yp)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=clf.classes_)
+disp.plot()
+plt.show()
