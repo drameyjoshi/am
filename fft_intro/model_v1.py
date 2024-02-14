7e58b3ccd88f3cee9592c7353e5b0dee0cf4e06f
drameyjoshi <amey.joshi@outlook.com>
2024-02-14 20:47:36 +0000

    Scaled inputs for logistic regression.

---
+++ fft_intro/model_v1.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.utils.validation import column_or_1d
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score
+from sklearn.preprocessing import StandardScaler
+
+training_data = 'crows/blr_gau_train.csv'
+test_data = 'crows/blr_gau_test.csv'
+scaler = StandardScaler().set_output(transform='pandas')
+
+orig_data = pd.read_csv(training_data)
+orig_data.columns = ['mean', 'std', 'min', 'q1', 'q2', 'q3', 'max', 'who']
+X = orig_data[['min', 'q1', 'mean', 'q3', 'max']]
+Xs = scaler.fit_transform(X)
+y = column_or_1d(orig_data[['who']])
+clf = LogisticRegression(random_state=0,
+                         max_iter=1000,
+                         solver='liblinear',
+                         penalty='l2').fit(Xs, y)
+print(f"Model score = {clf.score(X, y)}.")
+
+test_data = pd.read_csv(test_data)
+test_data.columns = ['mean', 'std', 'min', 'q1', 'q2', 'q3', 'max', 'who']
+X1 = test_data[['min', 'q1', 'mean', 'q3', 'max']]
+X1s = scaler.fit_transform(X1)
+y1 = column_or_1d(test_data[['who']])
+yp = clf.predict(X1s)
+
+accuracy = accuracy_score(y_true=y1, y_pred=yp)
+print(f"Accuracy score = {accuracy}.")
+cm = confusion_matrix(y_true=y1, y_pred=yp)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=clf.classes_)
+disp.plot()
+plt.show()
